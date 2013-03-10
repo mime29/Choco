88b6927e3c49363e25c3dbadca885308538199e6
Mikael Le Goff <mikaellegoff@gmail.com>
2013-03-10 16:30:41 +0900

    Centered gallery and bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120113522) do
+ActiveRecord::Schema.define(:version => 20130304111823) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
